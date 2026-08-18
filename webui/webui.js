5d475fe855c2fa465d42c95241ff19fdc0317cd8
Steven Wade <steven@stevenwade.co.uk>
2026-08-19 00:39:12 +0100

    feat(webui): add hardened multi-stage Dockerfile
    Signed-off-by: Steven Wade <steven@stevenwade.co.uk>

---
+++ webui/webui.js
@@ -0,0 +1,41 @@
+// Adapted from jpetazzo/container.training's dockercoins/webui/webui.js
+// (Apache 2.0). See NOTICE.
+import express from 'express';
+import morgan from 'morgan';
+import { createClient } from 'redis';
+
+var client = await createClient({
+  url: "redis://redis",
+  socket: {
+    family: 0
+  }
+})
+    .on("error", function (err) {
+        console.error("Redis error", err);
+    })
+    .connect();
+
+var app = express();
+
+app.use(morgan('common'));
+
+app.get('/', function (req, res) {
+    res.redirect('/index.html');
+});
+
+app.get('/json', async(req, res) => {
+    var coins = await client.hLen('wallet');
+    var hashes = await client.get('hashes');
+    var now = Date.now() / 1000;
+    res.json({
+        coins: coins,
+        hashes: hashes,
+        now: now
+    });
+});
+
+app.use(express.static('files'));
+
+var server = app.listen(80, function () {
+    console.log('WEBUI running on port 80');
+});
